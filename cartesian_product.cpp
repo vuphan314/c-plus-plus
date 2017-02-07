6c6c6149fe4026078982e78a2d12111d67fbe494
Vu Phan <vuphan314@gmail.com>
2017-02-06 23:41:41 -0600

    minor again

--- cartesian_product.cpp
+++ cartesian_product.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 ////////////////////////////////////////////////////////////
 
 template<typename T>
-vector<vector<T>> cart_product (const vector<vector<T>>& v) {
+vector<vector<T>> get_cartesian_product (
+    const vector<vector<T>> &v) {
   vector<vector<T>> s = {{}};
   for (auto& u : v) {
     vector<vector<T>> r;
@@ -25,13 +24,12 @@ vector<vector<T>> cart_product (const vector<vector<T>>& v) {
 ////////////////////////////////////////////////////////////
 
 int main () {
-	vector<vector<int> > test{{1}, {4,5,6}, {8,9}};
-	vector<vector<int> > res = cart_product(test);
-	for(size_t i = 0; i < res.size();i++){
-		for (size_t j = 0; j < res[i].size(); j++){
+	vector<vector<int>> test = {{1}, {4,5,6}, {8,9}};
+	vector<vector<int>> res = get_cartesian_product(test);
+	for (size_t i = 0; i < res.size(); i++) {
+		for (size_t j = 0; j < res[i].size(); j++) {
 			cout << res[i][j] << "\t";
 		}
-		cout << std::endl;
+		cout << endl;
 	}
-  return 0;
 }
