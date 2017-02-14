865f47815a7030e66664ef49a2d4237b861da902
Vu Phan <vuphan314@gmail.com>
2017-02-14 11:15:16 -0600

    split into header & cc

--- src/vectors.cc
+++ src/vectors.cc
@@ -1,19 +1,3 @@
-#include <iostream>
-#include <vector>
-
-////////////////////////////////////////////////////////////
-
-template<typename T>
-void printVector3d(const vector<vector<vector<T>>> &vector3d);
-
-template<typename T>
-void printVector2d(const vector<vector<T>> &vector2d);
-
-template<typename T>
-void printVector1d(const vector<T> &vector1d);
-
-////////////////////////////////////////////////////////////
-
 template<typename T>
 void printVector3d(const vector<vector<vector<T>>> &vector3d) {
   cout << "<\n";
---
+++ src/vectors.h
@@ -0,0 +1,17 @@
+#ifndef VECTORS_H_
+#define VECTORS_H_
+
+////////////////////////////////////////////////////////////
+
+template<typename T>
+void printVector3d(const vector<vector<vector<T>>> &vector3d);
+
+template<typename T>
+void printVector2d(const vector<vector<T>> &vector2d);
+
+template<typename T>
+void printVector1d(const vector<T> &vector1d);
+
+////////////////////////////////////////////////////////////
+
+#endif // VECTORS_H_
